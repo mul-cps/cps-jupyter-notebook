87eccec06890364d78b11d34243a81228317f058
Björn Ellensohn <bjoern.ellensohn@gmail.com>
2025-12-09 10:12:04 +0100

    fix: prevent script failure when changing user directory permissions by making `chown` commands non-critical.

--- docker/fix-permissions.sh
+++ docker/fix-permissions.sh
@@ -5,10 +5,10 @@ set -e
 # These might be mounted as root by Docker/K8s
 if [ -d "/home/${NB_USER}/.config" ]; then
     echo "Fixing permissions for /home/${NB_USER}/.config"
-    chown -R "${NB_USER}:users" "/home/${NB_USER}/.config"
+    chown -R "${NB_USER}:users" "/home/${NB_USER}/.config" || true
 fi
 
 if [ -d "/home/${NB_USER}/work" ]; then
     echo "Fixing permissions for /home/${NB_USER}/work"
-    chown -R "${NB_USER}:users" "/home/${NB_USER}/work"
+    chown -R "${NB_USER}:users" "/home/${NB_USER}/work" || true
 fi
